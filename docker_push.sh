ab68db6ef462e05daf67cfd1114245a10862ee21
Anonym Elev <anonymelev1234@gmail.com>
2019-11-04 22:18:50 +0100

    Test travis build

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git ls-files -o
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
 docker build . --tag application --build-arg JAR_FILE=./target/application-0.0.1-SNAPSHOT.jar
 docker tag application "${DOCKER_USERNAME}"/application
