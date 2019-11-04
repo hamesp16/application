631bb29f5f31eecb7a1e55fd9d390dd4444b8d89
Anonym Elev <anonymelev1234@gmail.com>
2019-11-04 21:10:07 +0100

    Docker travis build

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
-FROM openjdk:8-jre-alpine
-ENV APP_ROOT /app
-RUN mkdir ${APP_ROOT}
-WORKDIR ${APP_ROOT}
-COPY target/*.jar ${APP_ROOT}/application-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java", "-jar", "application-0.0.1-SNAPSHOT.jar"]
+FROM maven:8-jdk-alpine
+VOLUME /tmp
+ARG JAR_FILE
+COPY ${JAR_FILE} target/application-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","application-0.0.1-SNAPSHOT.jar"]
--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+git ls-files -o
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
-docker build . --tag application-image --build-arg JAR_FILE=./target/application-0.0.1-SNAPSHOT.jar
-docker tag application "${DOCKER_USERNAME}"/application
+docker build . --tag application-image --build-arg JAR_FILE=./build/libs/application-0.0.1-SNAPSHOT.jar
+docker tag application-image "${DOCKER_USERNAME}"/application
 docker push "${DOCKER_USERNAME}"/application
