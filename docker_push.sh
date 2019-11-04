9fd19ee34ae0e1acc56bdbedcb311392d66a3ad4
Anonym Elev <anonymelev1234@gmail.com>
2019-11-04 20:11:38 +0100

    Test Docker image build

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
-docker build . --tag application --build-arg JAR_FILE=./target/application-0.0.1-SNAPSHOT.jar
-docker tag application "${DOCKER_USERNAME}"/exam
-docker push "${DOCKER_USERNAME}"/exam
+docker build . --tag application-image --build-arg JAR_FILE=./target/application-0.0.1-SNAPSHOT.jar
+docker tag application "${DOCKER_USERNAME}"/application
+docker push "${DOCKER_USERNAME}"/application
